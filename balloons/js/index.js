b5a478188fa1f809a5b7827b4f60e4e7383da587
Kris Maini <krma@nodes.dk>
2016-04-26 17:16:30 +0200

    scroll with balloon animation

--- balloons/js/index.js
+++ balloons/js/index.js
@@ -12,17 +12,21 @@ function animateBallon1() {
   $("#balloon1").toggleClass("active", 1000, "easeOutSine");
   if (balloon1On) {
     $("#content1").fadeTo(2000, 1, function() {
-      $('html, body').animate({
-        scrollTop: $(".box1").offset().top-50
-      }, 200);
       $(".subline1").fadeTo(500, 1, function() {
         $(".box1").fadeTo(250, 1);
       });
     });
+    $('html, body').animate({
+      scrollTop: $(".box1").offset().top-50
+    }, 2000);
   } else {
     $('html, body').animate({scrollTop: 0}, 200);
     $(".subline1").fadeTo(100, 0);
     $("#content1").fadeTo(200, 0);
+
+    $(".content").css({
+      'pointer-events': 'none'
+    });
   }
 }
 
@@ -32,9 +36,6 @@ function animateBallon2() {
   $("#balloon2").toggleClass("active", 1000, "easeOutSine");
   if (balloon2On) {
     $("#content2").fadeTo(2000, 1, function(){
-      $('html, body').animate({
-        scrollTop: $(".box2").offset().top-50
-      }, 200);
       $(".subline2").fadeTo(500, 1, function() {
         $(".box2").fadeTo(250, 1);
         $(".subline2-1").fadeTo(1000, 1, function(){
@@ -42,10 +43,17 @@ function animateBallon2() {
         });
       });
     });
+    $('html, body').animate({
+      scrollTop: $(".box2").offset().top-50
+    }, 2000);
   } else {
     $('html, body').animate({scrollTop: 0}, 200);
     $(".subline2").fadeTo(100, 0);
     $("#content2").fadeTo(100, 0);
+
+    $(".content").css({
+      'pointer-events': 'none'
+    });
   }
 }
 
@@ -55,21 +63,28 @@ function animateBallon3() {
   $("#balloon3").toggleClass("active", 1000, "easeOutSine");
   if (balloon3On) {
     $("#content3").fadeTo(2000, 1, function(){
-      $('html, body').animate({
-        scrollTop: $(".box3").offset().top-50
-      }, 200);
       $(".subline3").fadeTo(500, 1, function() {
         $(".box3").fadeTo(250, 1);
       });
     });
+    $('html, body').animate({
+      scrollTop: $(".box3").offset().top-50
+    }, 2000);
   } else {
     $('html, body').animate({scrollTop: 0}, 200);
     $(".subline3").fadeTo(100, 0);
     $("#content3").fadeTo(100, 0);
+
+    $(".content").css({
+      'pointer-events': 'none'
+    });
   }
 }
 
 function closeOthers(balloon) {
+  $(".content").css({
+    'pointer-events': 'auto'
+  });
   switch(balloon) {
     case 1:
           if(balloon2On) {
